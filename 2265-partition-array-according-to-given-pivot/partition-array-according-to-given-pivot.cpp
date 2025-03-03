b7c9806f365bfb1d9d9cd009970640a289b7c013
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-03-04 04:36:19 +0530

    Time: 0 ms (100.00%) | Memory: 132.7 MB (68.41%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less, equal, greater;
+       stable_partition(nums.begin(), nums.end(), [pivot](int x) { return x < pivot; });
+    stable_partition(nums.begin(), nums.end(), [pivot](int x) { return x <= pivot; });
+    return nums;
 
-    for (int num : nums) {
-        if (num < pivot) less.push_back(num);
-        else if (num == pivot) equal.push_back(num);
-        else greater.push_back(num);
-    }
-
-    less.insert(less.end(), equal.begin(), equal.end());
-    less.insert(less.end(), greater.begin(), greater.end());
-
-    return less;
     }
 };
