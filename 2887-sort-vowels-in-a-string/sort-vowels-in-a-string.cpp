263061a40ff82056d8c7241da93b321e8978bb0f
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-05-20 10:22:34 +0530

    Time: 54 ms (5.09%) | Memory: 17.7 MB (20.35%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    string sortVowels(string s) {
-        priority_queue<char, vector<char>, greater<char>> pq;
-for(int i=0;i<s.size();i++){
-    if(s[i]=='A'||s[i]=='a'||s[i]=='I'||s[i]=='i'||s[i]=='O'||s[i]=='o'||s[i]=='U'||s[i]=='u'||s[i]=='E'||s[i]=='e'){
-        pq.push(s[i]);
+   string sortVowels(string s) {
+    unordered_set<char> vowels = {'a','e','i','o','u','A','E','I','O','U'};
+    priority_queue<char, vector<char>, greater<char>> pq;
+
+    for (char ch : s) {
+        if (vowels.count(ch)) pq.push(ch);
     }
+
+    for (char &ch : s) {
+        if (vowels.count(ch)) {
+            ch = pq.top();
+            pq.pop();
+        }
+    }
+
+    return s;
 }
-for(int i=0;i<s.size();i++){
-    if(s[i]=='A'||s[i]=='a'||s[i]=='I'||s[i]=='i'||s[i]=='O'||s[i]=='o'||s[i]=='U'||s[i]=='u'||s[i]=='E'||s[i]=='e'){
-        s[i]=pq.top();
-        pq.pop();
-    }
-}
-return s;
-    }
+
 };
