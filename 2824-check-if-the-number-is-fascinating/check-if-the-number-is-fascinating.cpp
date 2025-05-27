c00762ae5cb5322b7125978c05c91238a415bedb
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-05-27 07:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.89%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-      int n1=2*n,n2=3*n;
-      string str;
-      str+=to_string(n);
-      str+=to_string(n1);
-      str+=to_string(n2);
-      unordered_map<char,int>mpp;
-      for(int i=0;i<str.size();i++){
-        mpp[str[i]]++;
-      }
-      if(mpp.find('0')!=mpp.end()){
-        return false;
-      }
-      for(auto it:mpp){
-        if(it.second>1){
-            return false;
-        }
-      }
-      return true;
+     string str= to_string(n)+to_string(2*n)+to_string(3*n);
+        sort(str.begin(),str.end());
+        return str == "123456789";
     }
 };
