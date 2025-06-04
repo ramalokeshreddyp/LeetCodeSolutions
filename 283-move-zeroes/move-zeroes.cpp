f6e27c12fce1e97c75fd1109c11fb03b08230c92
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-06-04 06:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.26%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        stable_partition(nums.begin(),nums.end(),[](int c){return c!=0;});
+    int in1=-1;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==0&&in1==-1){
+            in1=i;
+        }
+       if(nums[i]!=0)
+       {
+        if(in1!=-1){
+            swap(nums[i],nums[in1]);
+            in1++;
+        }
+       }
+    }
+
     }
 };
