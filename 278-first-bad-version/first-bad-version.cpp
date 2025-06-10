dc7a983bf9acb609c46f10c126c6828768daa6bd
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-06-10 10:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.92%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,18 +4,18 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-         int left = 1, right = n;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2; 
-            
-            if (isBadVersion(mid)) {  
-                right = mid;
-            } else { 
-                left = mid + 1;
+        int low=1,high=n;
+        int result=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isBadVersion(mid)){
+                result=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        
-        return left;  
+return result;
     }
 };
