86b0a6541f18fe1edc46485b3f3906918fe1b624
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-06-16 10:39:22 +0530

    Time: 11 ms (5.15%) | Memory: 97.2 MB (99.09%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+      int result=0;
+     int n=nums.size();
+   int xmin=nums[0];
+   for(int i=1;i<n;i++){
+    
+    if(nums[i]<=xmin){
+        xmin=nums[i];
+    }
+    else{
+        result=max(result,abs(xmin-nums[i]));
+    }
+   }
+    return result;   
+    }
+};
