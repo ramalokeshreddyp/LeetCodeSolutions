0fa1d6ed28ac29b164aa6de34cd14a0eaa7d4470
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-06-23 10:45:39 +0530

    Time: 6 ms (40.66%) | Memory: 22 MB (81.03%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-    bool validPalindrome(string s) {
-      int n=s.size();
-      int low=0,high=n-1;
-      while(low<high){
-        if(s[low]==s[high]){
-            low++;
-            high--;
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
 
+    bool validPalindrome(string s) {
+        int low = 0, high = s.size() - 1;
+
+        while (low < high) {
+            if (s[low] == s[high]) {
+                low++;
+                high--;
+            } else {
+               
+                return isPalindrome(s, low + 1, high) || isPalindrome(s, low, high - 1);
+            }
         }
-        else{
-            break;
-        }
-      }
-if(low==high||abs(low-high)==1){
-    return true;
-}
-string str1=s.substr(low+1,high-low);
-string str2=s.substr(low,high-low);
-int f=1;
-for(int i=0;i<str1.size()/2;i++){
-if(str1[i]!=str1[str1.size()-1-i]){
-  f=0;
-  break;
-}
-}
-int g=1;
-for(int i=0;i<str2.size()/2;i++){
-if(str2[i]!=str2[str2.size()-1-i]){
-    g=0;
-    break;
-}
-}
-if(f==1||g==1){
-    return true;
-}
-return false;
+        return true;
     }
 };
