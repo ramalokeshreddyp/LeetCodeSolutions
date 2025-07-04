6f74791a65623531735a33ae8e7bf13474078fb1
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-07-04 20:43:17 +0530

    Time: 8 ms (51.73%) | Memory: 128.3 MB (79.44%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       stable_partition(nums.begin(),nums.end(),[](int x){return x>0;}); 
-       vector<int>res;
-       int n=nums.size();
-       for(int i=0;i<n/2;i++){
-        res.push_back(nums[i]);
-        res.push_back(nums[n/2+i]);
-       }
-       return res;
+        vector<int> res(nums.size(), 0);
+        int posIndex=0, negIndex=1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<0){
+                res[negIndex]=nums[i];
+                negIndex +=2;
+            }
+            else{
+                res[posIndex]=nums[i];
+                posIndex +=2;
+            }
+        }
+        return res;
     }
 };
