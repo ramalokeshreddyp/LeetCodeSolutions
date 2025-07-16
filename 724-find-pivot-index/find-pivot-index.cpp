f35f59569d9c3163860e65126b173f50ad91eeaa
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-07-16 10:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (51.26%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+       
+       int sum=accumulate(nums.begin(),nums.end(),0) ;
+       int lsum=0;
+       for(int i=0;i<nums.size();i++){
+        sum-=nums[i];
+        if(lsum==sum){
+            return i;
+        }
+        lsum+=nums[i];
+       }
+       return -1;
+    }
+};
