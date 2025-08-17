270aa4cd3f74d95999012b1107e8048b438ef1de
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-08-17 06:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (99.90%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-       bitset<32>b(n); 
+        bitset<32> b(n);
+        unsigned long x = b.to_ulong();
+        int last = (x > 0) ? 31 - __builtin_clz(x) : -1;
 
-    int last = -1; 
-    for (int i = 31; i >= 0; i--) {  
-        if (b.test(i)) {
-            last = i;
-            break;
+        for (int i = 0; i < last; i++) {
+            if (b[i] == b[i + 1]) return false;
         }
-    } 
-    for(int i=0;i<last;i++){
-        if(b[i]==b[i+1]){
-            return false;
-        }
-    }
-    return true;
+        return true;
     }
 };
