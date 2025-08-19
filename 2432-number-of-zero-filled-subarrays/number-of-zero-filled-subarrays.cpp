62127e78b92bc638bb610ad8ef45fd258911b9fc
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-08-19 19:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 111.3 MB (40.61%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+    
+        long cnt = 0, streak = 0;
+        for (int num : nums) {
+            streak = (num == 0) ? streak + 1 : 0;
+            cnt += streak;
+        }
+        return cnt;
+    
+
+    }
+};
