5879715cab5405c1f427c10b4a34da632fa749e2
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-02-25 08:47:10 +0530

    Time: 4 ms (32.29%) | Memory: 14.8 MB (32.46%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>res(n);
-        for(int i=0;i<n;i++){
-            res[i]=__builtin_popcount(arr[i]);
+        vector<pair<int,int>>res;
+        for(auto it:arr){
+            res.push_back({__builtin_popcount(it),it});
+            
         }
-        for(int i=0;i<n-1;i++){
-for(int j=0;j<n-1-i;j++){
-    if(res[j]>res[j+1]){
-        int temp=res[j];
-        res[j]=res[j+1];
-        res[j+1]=temp;
-        temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-    }
-    else if(res[j]==res[j+1]&&arr[j]>arr[j+1]){
-              int   temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-    }
-}
+        sort(res.begin(),res.end());
+        vector<int>ans;
+        for(auto it:res){
+            ans.push_back(it.second);
         }
-        return arr;
+        return ans;
+        
     }
 };
