36fd956c2e6f40f2ae959b985fbf9665341f3ef9
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-03-09 11:04:03 +0530

    Time: 41 ms (64.15%) | Memory: 84.7 MB (83.00%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,23 +3,23 @@ public:
     int totalFruit(vector<int>& fruits) {
         int n=fruits.size();
         int mx=0;
-       int l=0,r=0;
-       unordered_map<int,int>mpp;
-       while(r<n){
+     int l=0,r=0;
+     unordered_map<int,int>mpp;
+     while(r<n){
 mpp[fruits[r]]++;
-if(mpp.size()>2){
+while(mpp.size()>2){
     mpp[fruits[l]]--;
     if(mpp[fruits[l]]==0){
         mpp.erase(fruits[l]);
     }
     l++;
 }
-
-        if(mpp.size()<=2){
-            mx=max(mx,r-l+1);
-        }
+if(mpp.size()<=2){
+    mx=max(mx,r-l+1);
+}
 r++;
-       }
-        return mx;
+     }
+     return mx;
+
     }
 };
