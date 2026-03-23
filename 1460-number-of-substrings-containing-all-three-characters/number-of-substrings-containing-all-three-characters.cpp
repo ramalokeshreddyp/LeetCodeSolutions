fe6f6699a6841bbefa44ca0ec3d9a7edc3a20fda
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-03-23 06:12:43 +0530

    Time: 15 ms (31.43%) | Memory: 11.1 MB (56.18%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,11 +5,11 @@ public:
   int hash[3]={-1,-1,-1};
   for(int i=0;i<s.size();i++){
     hash[s[i]-'a']=i;
-if(hash[0]!=-1&&hash[1]!=-1&&hash[2]!=-1){
+
     cnt+=(1+min({hash[0],hash[1],hash[2]}));
-}
+
   }
   return cnt;
-  
+
     }
 };
