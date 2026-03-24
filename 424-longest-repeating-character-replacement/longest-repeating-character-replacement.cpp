e5fcc9dd8f583ff1a0e583b1040641a2cc091907
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-03-24 05:50:08 +0530

    Time: 3 ms (66.49%) | Memory: 10.8 MB (69.79%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int l=0,r=0,maxl=0,maxf=0;
+        int hash[26]={0};
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxf=max(maxf,hash[s[r]-'A']);
+            if((r-l+1)-maxf>k){
+                hash[s[l]-'A']--;
+                maxf=0;
+                l++;
+            }
+            if((r-l+1)-maxf<=k){
+                maxl=max(maxl,r-l+1);
+            }
+            r++;
+        }
+        return maxl;
+    }
+};
