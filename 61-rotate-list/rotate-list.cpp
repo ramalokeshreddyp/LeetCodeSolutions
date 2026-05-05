9e7f0d0454f0e324ac94f4cfe508ce6ead55ab83
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-05-05 09:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.20%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,46 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-       if (!head || k == 0) return head;
-
-    vector<int> res;
-    ListNode *temp = head;
-
-    while (temp != nullptr) {
-        res.push_back(temp->val);
-        temp = temp->next;
-    }
-
-    int s = res.size();
-    k = k % s;
-    if (k == 0) return head;
-
-    int req = s - k;
-    std::rotate(res.begin(), res.begin() + req, res.end());
-
-    ListNode *headd = nullptr;
-    temp = nullptr;
-
-    for (int i = 0; i < res.size(); i++) {
-        ListNode *newnode = new ListNode(res[i]);
-        if (headd == nullptr) {
-            headd = temp = newnode;
-        } else {
-            temp->next = newnode;
-            temp = newnode;
+        if (!head || !head->next || k == 0) return head;
+        
+        ListNode* tail = head;
+        int n = 1;
+        while (tail->next) {
+            tail = tail->next;
+            n++;
         }
-    }
-
-    return headd;
+        
+        k %= n;
+        if (k == 0) return head;
+        
+        tail->next = head;
+        int steps = n - k;
+        ListNode* newTail = head;
+        
+        for (int i = 1; i < steps; i++) newTail = newTail->next;
+        
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+        
+        return newHead;
     }
 };
