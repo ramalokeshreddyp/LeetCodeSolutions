f6e22ba18949a97a670668f558d6c0eacb381c6b
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-05-14 06:21:48 +0530

    Time: 6 ms (46.17%) | Memory: 10.9 MB (69.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n=s.size();
-        int l=0,r=0,maxl=0,maxf=0;
-        int hash[26]={0};
-        while(r<n){
-            hash[s[r]-'A']++;
-            maxf=max(maxf,hash[s[r]-'A']);
-            if((r-l+1)-maxf>k){
-                hash[s[l]-'A']--;
-                maxf=0;
-               /* for(auto it:hash){
-                    maxf=max(maxf,it);
-                }*/
-                l++;
+     int maxl=0;
+     int n=s.size();
+     int l=0,r=0,maxf=0;
+     int hash[26]={0};
+     while(r<n){
+        hash[s[r]-'A']++;
+        maxf=max(maxf,hash[s[r]-'A']);
+        while((r-l+1)-maxf>k){
+            hash[s[l]-'A']--;
+            maxf=0;
+            for(int i=0;i<26;i++){
+                maxf=max(maxf,hash[i]);
             }
-            if((r-l+1)-maxf<=k){
-                maxl=max(maxl,r-l+1);
-            }
-            r++;
+            l++;
         }
-        return maxl;
+        if((r-l+1)-maxf<=k){
+            maxl=max(maxl,r-l+1);
+        }
+        r++;
+     }
+     return maxl;
     }
 };
