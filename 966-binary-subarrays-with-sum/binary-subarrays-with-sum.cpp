902d22a7e3d71e735a65d7003c71eb6f4b107397
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-05-16 05:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (82.21%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+int func(vector<int>&a,int k){
+    if(k<0){
+        return 0;
+    }
+    int n=a.size();
+    int l=0,r=0,sum=0,cnt=0;
+    while(r<n){
+        sum+=a[r];
+        while(sum>k){
+            sum-=a[l];
+            l++;
+        }
+        cnt=cnt+(r-l+1);
+        r++;
+    }
+    return cnt;
+}
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       int n=nums.size();
-       int cnt=0;
-      map<int,int>mpp;
-      int p=0;
-      mpp[0]=1;
-      for(int i=0;i<n;i++){
-        p+=nums[i];
-        int r=p-goal;
-        cnt+=mpp[r];
-        mpp[p]+=1;
-      }
-        return cnt;
-       
+       return func(nums,goal)-func(nums,goal-1);
     }
 };
