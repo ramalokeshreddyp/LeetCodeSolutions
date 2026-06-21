643c83bbf8fb31d95e94c91a5692dcd85321062c
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-06-21 05:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (25.45%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -3,11 +3,12 @@ public:
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int cnt=0;
-     for(int i=0;i<32;i++){
-        if(ans&(1<<i)){
+    while(ans>0){
+        if(ans&1){
             cnt++;
         }
-     }
+        ans=ans>>1;
+    }
         return cnt;
     }
 };
