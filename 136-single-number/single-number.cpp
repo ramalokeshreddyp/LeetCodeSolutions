82dfc4e4cfa916ce18ee14b37e2bd80263610c59
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-06-21 05:54:24 +0530

    Time: 5 ms (19.03%) | Memory: 23.9 MB (7.45%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-       for(int i=0;i<nums.size();i++){
-        res^=nums[i];
-       }
-        return res;
+      unordered_map<int,int>mpp;
+      for(auto it:nums)  {
+        mpp[it]++;
+      }
+      for(auto it:mpp){
+        if(it.second==1){
+            return it.first;
+        }
+      }
+      return 0;
     }
 };
