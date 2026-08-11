c02af1dd6f410872462dc927bb441042a2195751
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-08-11 15:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.66%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,7 +3,7 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
          if (n == 1) return nums[0];
-        vector<int> dp(n);
+        int dp[n];
         dp[0]=nums[0];
         dp[1]=max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
