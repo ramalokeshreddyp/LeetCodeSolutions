214115652d62319c75fcc1b3ac33812984f4e6d6
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-08-14 05:25:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (74.01%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -14,14 +14,20 @@ if(dividend<=0&&divisor>0){
 long n=abs((long)dividend);
 long d=abs((long)divisor);
 long ans=0;
-while(n>=d){
+/*while(n>=d){
     int cnt=0;
     while(n>=(d<<(cnt+1))){
         cnt++;
     }
     ans+=(1LL<<cnt);
     n-=(d<<cnt);
-}
+}*/
+for (int i = 31; i >= 0; i--) {
+            if ((d << i) <= n) {
+                n -= (d << i);
+                ans += (1LL << i);
+            }
+        }
 if(ans>=(1LL<<31)&&sign){
     return INT_MAX;
 }
