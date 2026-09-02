91455fc8d4c444c817c619ab3791acb629ca346c
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-09-02 10:29:00 +0530

    Time: 1 ms (10.91%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        /*int cnte=count_if(nums1.begin(),nums1.end(),[](int x){return x%2==0;});
+        int n=nums1.size();
+        int cnto=n-cnte;*/
+return true;
+    }
+};
